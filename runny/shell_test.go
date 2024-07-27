deb04cd1431941bb900b1011bfea5c01d5ebb15f
Simon Whitaker <sw@netcetera.org>
2024-07-27 14:48:28 +0100

    100% test coverage in shell

--- runny/shell_test.go
+++ runny/shell_test.go
@@ -13,3 +13,36 @@ func TestNewShell(t *testing.T) {
 		t.Errorf("Expected an error")
 	}
 }
+
+func TestPosixShellRun(t *testing.T) {
+	ps := PosixShell{command: "/bin/bash"}
+	err := ps.Run("ls", []string{}, false, false, []string{})
+	if err != nil {
+		t.Errorf("Expected success, got an error: %v", err)
+	}
+
+	err = ps.Run("ls", []string{"/"}, false, false, []string{})
+	if err != nil {
+		t.Errorf("Expected success when passing a valid arg, got an error: %v", err)
+	}
+
+	err = ps.Run("ls", []string{}, true, false, []string{})
+	if err != nil {
+		t.Errorf("Expected success when echoing stdout, got an error: %v", err)
+	}
+
+	err = ps.Run("ls", []string{}, false, true, []string{})
+	if err != nil {
+		t.Errorf("Expected success when running in verbose mode, got an error: %v", err)
+	}
+
+	err = ps.Run("ls", []string{}, false, false, []string{"FOO=foo"})
+	if err != nil {
+		t.Errorf("Expected success when passing env variables, got an error: %v", err)
+	}
+
+	err = ps.Run("ls", []string{"does-not-exist"}, false, false, []string{})
+	if err == nil {
+		t.Errorf("Expected error if the command fails, but no error was returned")
+	}
+}
