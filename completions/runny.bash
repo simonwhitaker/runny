5e7ce68cd74f2175d34c7cc688cef31f283baba2
Simon Whitaker <sw@netcetera.org>
2024-08-04 09:13:50 +0100

    Fix bash completion

--- completions/runny.bash
+++ completions/runny.bash
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-complete -W "$(runny | cut -f1)" runny
+# See https://opensource.com/article/18/3/creating-bash-completion-script
+_runny_completions() {
+    runny_commands=$(runny | cut -f1)
+    COMPREPLY=($(compgen -W "${runny_commands}" "${COMP_WORDS[1]}"))
+}
+
+complete -F _runny_completions runny
